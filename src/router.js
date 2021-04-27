58558d8da9df1860e910dc342a60ee32bde01059
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-04-27 21:00:12 +0200

    Retrieve some images when loading

--- src/router.js
+++ src/router.js
@@ -11,7 +11,7 @@ const router = new Router({
       path: '/',
       name: 'welcome',
       component: () => import('./views/Welcome.vue'),
-    }
+    },
   ],
 })
 
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,6 +1,32 @@
 <template lang="pug">
-    section
-        h1 {{ $t("welcome.welcome") }}
-        //.text-center
-        //  img(src="@/assets/images/background.png")
+section
+    h3(v-if='loading' ) Loading...
+    div(v-else)
+        p Got some images
 </template>
+<script>
+import { getImages } from '@/apis/apis'
+import { errorToast } from '@/helpers/ui'
+export default {
+  data: function () {
+    return {
+      images: [],
+      loading: true,
+    }
+  },
+  mounted: function () {
+    this.loadImages()
+  },
+  methods: {
+    async loadImages() {
+      try {
+        this.images = await getImages()
+      } catch (err) {
+        this.$bvToast.toast(`Images can't be retrieved`, errorToast)
+      } finally {
+        this.loading = false
+      }
+    },
+  },
+}
+</script>
